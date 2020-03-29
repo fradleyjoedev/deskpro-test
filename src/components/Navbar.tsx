b140cafe6b3dfea874a7c34e067a761e4587da62
Joe Fradley <jfradley@sortable.co.uk>
2020-03-29 14:56:09 +0100

    Set document title based on the page selected

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,12 +6,16 @@ interface IProps {
   links: IRouteData[];
 }
 
+const setDocumentTitle = (title: string) => {
+  document.title = title;
+}
+
 export const NavBar: React.FunctionComponent<IProps> = ({links}): JSX.Element => {
   return (
     <ul>
       {links.map((route: IRouteData, i: number) => {
-          return (<li>
-            <Link to={route.path}>{route.linkTitle}</Link>
+          return (<li key={i}>
+            <Link to={route.path} onClick={() => setDocumentTitle(route.pageTitle)}>{route.linkTitle}</Link>
           </li>)
       })}
     </ul>
